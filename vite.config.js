4098144b47c2ea87cb5180162087746a0e50fa6f
vadym4che <vadym4che@gmail.com>
2023-10-28 07:37:32 +0300

    implement CI - take number 20

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ command, mode, ssrBuild }) => {
+export default defineConfig(({
+  command,
+  // mode,
+  // ssrBuild,
+}) => {
   if (command === 'serve') {
     return {
       plugins: [react()],
